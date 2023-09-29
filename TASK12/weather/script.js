f36951d4fd67bb180f5216bdb89a9ab08b6d59dc
partha2001ps <parthapn2017@gmail.com>
2023-09-29 17:46:18 +0530

    task12

---
+++ TASK12/weather/script.js
@@ -0,0 +1,32 @@
+//    to get class name in html
+   let palce = document.querySelector(".place")
+    let DisplayWeather=document.querySelector(".DisplayWeather")
+let detailes = document.querySelector(".detailes")
+
+    // to click function for the weather detalies to get the api values
+        DisplayWeather.addEventListener('click',(event)=>{
+        event.preventDefault();
+// to enter the name city is store by Name
+        const Name=(palce.value)
+        
+        const apiKey = "cc86f1689f25fe0c147c87a9e570177c";
+            // to api for the weather
+            
+        fetch(`https://api.openweathermap.org/data/2.5/weather?q=${Name}&appid=${apiKey}&units=metric`)
+        .then((res) => res.json())
+        .then((data) => {
+        detailes.innerHTML="";
+// temperature and main from the api weather
+        const temperature = data.main.temp;
+        let weat=data.weather[0].main
+            console.log(temperature);
+            // to create the element temprature
+        let temp=document.createElement('p')
+        temp.innerHTML=`<p>Temperature : ${temperature}℃</p><p>☁️${weat}</p>`
+        detailes.appendChild(temp)
+
+        })
+        .catch(error=>{
+        console.log('enter correct',error)
+})
+})
